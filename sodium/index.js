882a5e114084b1d7591bcbf3fde4fc911ac635b2
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-06-16 19:18:42 -0400

    js server that responds with bins from sodium generation

--- sodium/index.js
+++ sodium/index.js
@@ -1,15 +1,35 @@
 const { SodiumPlus } = require("sodium-plus");
-(async function() {
+
+async function randomNum() {
     // Select a backend automatically
     let sodium = await SodiumPlus.auto();
  
-    let key = await sodium.crypto_secretbox_keygen();
-    let nonce = await sodium.randombytes_buf(24);
-    let message = 'This is just a test message';
-    // Message can be a string, buffer, array, etc.
- 
-    let ciphertext = await sodium.crypto_secretbox(message, nonce, key);
-    console.log(ciphertext);
-    let decrypted = await sodium.crypto_secretbox_open(ciphertext, nonce, key);
-    console.log(decrypted.toString('utf-8'));
-})();
+    let num = await sodium.randombytes_uniform(256);
+    return num
+}
+
+/** @type {async (n: number) => number[]} */
+async function binRandomNums(n) {
+    /** @type {number[]} */
+    let bins = Array(16).fill(0);
+    for (let i = 0; i < n; ++i) {
+        const n = await randomNum();
+        const q = Math.trunc(n/16);
+        bins[q] += 1;
+    }
+    return bins;
+}
+
+const express = require("express");
+const app = express();
+
+app.get("/:n", async (req, res) => {
+    const n = Number.parseInt(req.params.n);
+    const bins = await binRandomNums(n);
+    res.status(200).json({ bins });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log("Sodium server listening on port", PORT);
+});
