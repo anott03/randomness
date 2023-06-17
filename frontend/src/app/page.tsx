e2a3e092c789816c4e4f32d046b475577e3898d6
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-06-17 02:55:01 -0400

    need to think about how to prevent spamming the quantum simulator with requests

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,33 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useQuery } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
+import { QueryClient, QueryClientProvider, useQueryClient, useQuery } from "@tanstack/react-query";
+import { useRef, useState } from "react";
 
 function PageContents() {
+  const queryClient = useQueryClient();
   const [ numNums, setNumNums ] = useState(10);
   const quantumQuery = useQuery({ queryKey: ["quantumNums"], queryFn: async () => {
       return await fetch(`http://localhost:3002/${numNums}`).then(res => res.json());
   }})
+  const formRef = useRef(null);
 
   const sodiumQuery = useQuery({ queryKey: ["sodiumNums"], queryFn: async () => {
       return await fetch(`http://localhost:3001/${numNums}`).then(res => res.json());
   }})
     return (
       <main className="w-screen h-screen flex flex-col p-3 bg-black text-white">
-          <form className="flex flex-row py-2 gap-2" onSubmit={(e) => {
+          <form className="flex flex-row py-2 gap-2" ref={formRef} onSubmit={(e) => {
               e.preventDefault();
+              fetch(`http://localhost:3002/${numNums}`)
+                .then(res => res.json())
+                .then(json => queryClient.setQueryData(["quantumQuery"], json));
+              fetch(`http://localhost:3001/${numNums}`)
+                .then(res => res.json())
+                .then(json => queryClient.setQueryData(["sodiumQuery"], json));
+              if (formRef && formRef.current) {
+                  // @ts-ignore
+                  formRef.current.disabled = true;
+              }
           }}>
               <input name="inpt" type="number" value={numNums} onChange={(e) => setNumNums(Number.parseInt(e.target.value))} className="p-2 bg-inherit text-inherit border border-white flex-1"/>
               <button type="submit" className="p-2 bg-violet-500 hover:bg-violet-600">Go</button>
