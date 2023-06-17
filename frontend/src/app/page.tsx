9a4c7af6c3e70047c8aa8da7fff4de25dffe291c
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-06-17 12:34:50 -0400

    frontend: proper data fetching

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,43 +1,73 @@
 "use client";
 
-import { QueryClient, QueryClientProvider, useQueryClient, useQuery } from "@tanstack/react-query";
-import { useRef, useState } from "react";
+import { QueryClient, QueryClientProvider, useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
+import { LegacyRef, useMemo, useRef, useState } from "react";
+
+const useMutateQuantum = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (n: number) => {
+            return await fetch(`http://localhost:3002/${n}`).then(res => res.json());
+        },
+        onSuccess: (data) => {
+            queryClient.setQueryData(["quantumNums"], data);
+        }
+    });
+}
+
+const useMutateSodium = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (n: number) => {
+            return await fetch(`http://localhost:3001/${n}`).then(res => res.json());
+        },
+        onSuccess: (data) => {
+            queryClient.setQueryData(["sodiumNums"], data);
+        }
+    });
+}
 
 function PageContents() {
-  const queryClient = useQueryClient();
   const [ numNums, setNumNums ] = useState(10);
+  const formRef: LegacyRef<HTMLFormElement> = useRef(null);
+
   const quantumQuery = useQuery({ queryKey: ["quantumNums"], queryFn: async () => {
       return await fetch(`http://localhost:3002/${numNums}`).then(res => res.json());
   }})
-  const formRef = useRef(null);
 
   const sodiumQuery = useQuery({ queryKey: ["sodiumNums"], queryFn: async () => {
       return await fetch(`http://localhost:3001/${numNums}`).then(res => res.json());
   }})
-    return (
-      <main className="w-screen h-screen flex flex-col p-3 bg-black text-white">
-          <form className="flex flex-row py-2 gap-2" ref={formRef} onSubmit={(e) => {
-              e.preventDefault();
-              fetch(`http://localhost:3002/${numNums}`)
-                .then(res => res.json())
-                .then(json => queryClient.setQueryData(["quantumQuery"], json));
-              fetch(`http://localhost:3001/${numNums}`)
-                .then(res => res.json())
-                .then(json => queryClient.setQueryData(["sodiumQuery"], json));
-              if (formRef && formRef.current) {
-                  // @ts-ignore
-                  formRef.current.disabled = true;
-              }
-          }}>
-              <input name="inpt" type="number" value={numNums} onChange={(e) => setNumNums(Number.parseInt(e.target.value))} className="p-2 bg-inherit text-inherit border border-white flex-1"/>
-              <button type="submit" className="p-2 bg-violet-500 hover:bg-violet-600">Go</button>
-          </form>
-          <p className="text-xl font-bold">Quantum Random Numbers</p>
-          { quantumQuery.isLoading ? <p className="text-xl">Loading...</p> : quantumQuery.data.bins.map((n: number, i:number) => <p key={i}>{n}</p>) }
-          <p className="text-xl font-bold">Sodium Random Numbers</p>
-          { sodiumQuery.isLoading ? <p className="text-xl">Loading...</p> : sodiumQuery.data.bins.map((n: number, i:number) => <p key={i}>{n}</p>) }
-      </main>
-    );
+
+  const quantumMutation = useMutateQuantum();
+  const sodiumMutation = useMutateSodium();
+
+  const dataLoading = useMemo(() =>
+      (quantumQuery.isLoading)||(sodiumQuery.isLoading)||(quantumMutation.isLoading)||(sodiumMutation.isLoading),
+      [quantumQuery, sodiumQuery, quantumMutation, sodiumMutation])
+
+  return (
+    <main className="w-screen h-screen flex flex-col p-3 bg-black text-white">
+        <form className="flex flex-row py-2 gap-2" ref={formRef} onSubmit={(e) => {
+            e.preventDefault();
+            quantumMutation.mutate(numNums);
+            sodiumMutation.mutate(numNums);
+        }}>
+            <input disabled={dataLoading} name="inpt" type="number" value={numNums} onChange={(e) => {
+                if (e.target.value && e.target.value !== "") {
+                    setNumNums(Number.parseInt(e.target.value))
+                } else {
+                    setNumNums(0);
+                }
+            }} className="p-2 bg-inherit text-inherit border border-white flex-1"/>
+            <button disabled={dataLoading} type="submit" className="p-2 bg-violet-500 hover:bg-violet-600">Go</button>
+        </form>
+        <p className="text-xl font-bold">Quantum Random Numbers</p>
+        { dataLoading ? <p className="text-xl">Loading...</p> : quantumQuery.data.bins.map((n: number, i:number) => <p key={i}>{n}</p>) }
+        <p className="text-xl font-bold">Sodium Random Numbers</p>
+        { dataLoading ? <p className="text-xl">Loading...</p> : sodiumQuery.data.bins.map((n: number, i:number) => <p key={i}>{n}</p>) }
+    </main>
+  );
 }
 
 export default async function Home() {
